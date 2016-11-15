b73943f79f0d3205ebfe546f0154e4aea22be063
Marcteen <Marcteen@MarcteendeMacBook-Pro.local>
2016-11-15 14:00:59 +0800

    work out another two

---
+++ src/leeTs/Easy/_14_Longest_Common_Prefix.java
@@ -0,0 +1,10 @@
+package leeTs.Easy;
+
+public class _14_Longest_Common_Prefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/leeTs/Easy/_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,10 @@
+package leeTs.Easy;
+
+public class _19_Remove_Nth_Node_From_End_of_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
