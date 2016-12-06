ad54d10c6c15bbfe84924edf9189a497c31490f2
gyllen <qpegasus@126.com>
2016-12-06 23:27:30 +0800

    dfj

--- src/leeTs/Easy/_191_Number_of_1_Bits.java
+++ src/leeTs/Easy/_191_Number_of_1_Bits.java
@@ -1,18 +1,19 @@
 package leeTs.Easy;
 
-public class _172_Factorial_Trailing_Zeroes {
+public class _191_Number_of_1_Bits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public int trailingZeroes(int n) {
+	public int hammingWeight(int n ) {
 		int result = 0;
 		while(n > 0) {
-			result += n / 5;
-			n /= 5;
+			result += (n & 1);
+			n >>>= 1;
 		}
 		return result;
 	}
+	
 }
