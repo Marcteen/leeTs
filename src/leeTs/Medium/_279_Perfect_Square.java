a29cb7c244ba0d5f1cf87ab4cf40fe73fa563288
Marcteen <qpegasus@126.com>
2017-06-20 21:17:13 +0800

    a

--- src/leeTs/Medium/_279_Perfect_Square.java
+++ src/leeTs/Medium/_279_Perfect_Square.java
@@ -6,5 +6,20 @@ public class _279_Perfect_Square {
 		// TODO Auto-generated method stub
 
 	}
-
+	/*the dp key: the solution of current number is surely can be construct
+    from the previous solution by adding a square!!!!*/
+    public int numSquares(int n) {
+        if(n < 2)
+            return n;
+        int[] dp = new int[n + 1];
+        for(int i = 1; i <= n; i ++)
+            dp[i] = Integer.MAX_VALUE;
+        /*when handle i, the number before i must have been processed*/
+        for(int i = 1; i <= n; i ++) {
+            for(int j = 1; j * j <= i; j ++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
 }
--- src/leeTs/Medium/_300_Longest_Increasing_Subsequence.java
+++ src/leeTs/Medium/_300_Longest_Increasing_Subsequence.java
@@ -6,5 +6,62 @@ public class _300_Longest_Increasing_Subsequence {
 		// TODO Auto-generated method stub
 
 	}
-
+	/*tricky dp using adding the tail element one by one*/
+	public int lengthOfLISDp(int[] nums) {
+		if(null == nums || 0 == nums.length) {
+            return 0;
+        }
+		int[] tails = new int[nums.length];
+		/* size means the number, tails[i] stores the least number of
+		 * the sub sequence of length i + 1*/
+		int low = 0, mid = 0, high = 0, size = 0;
+		for(int ni : nums) {
+			low = 0;
+			high = size;
+			while(low < high) {
+				mid = low + (high - low) / 2;
+				/*we need to find the first element that is
+				 * not less than ni(for the tail update, ni
+				 * is surely less than tail[low], otherwise
+				 * the loop will increase i, in this way 
+				 * we give the room to obtain longer solution)
+				 * or reach beyond the current longest sub
+				 * sequence, noted that the tail[size] is not obtained yet,
+				 * because current solution of size is coressponding to tails[size - 1]*/
+				if(tails[mid] < ni)
+					low = mid + 1;
+				// it may just at target, so move to it rather than skip it
+				else
+					high = mid;
+			}
+			/*now low(== high) must at the insert position, we can figure out
+			 * that tails must be ascend because the loop will skip the values
+			 * that less than ni*/
+			tails[low] = ni;
+			/*already get a new one, so expend it*/
+			if(low == size)
+				size ++;
+		}
+		return size;
+	}
+	
+	/*Naive prefix checking one by one*/
+	public int lengthOfLIS(int[] nums) {
+        if(null == nums || 0 == nums.length) {
+            return 0;
+        }
+        int[] length = new int[nums.length];
+        int max = 0;
+        for(int i = 0; i < nums.length; i ++) {
+            for(int j = 0; j < i; j ++) {
+                if(nums[i] > nums[j] && length[j] + 1 > length[i]) {
+                    length[i] = length[j] + 1;
+                    if(length[i] > max)
+                        max = length[i];
+                }
+            }
+        }
+        /*the values in length just one less than the real ones*/
+        return max + 1;
+    }
 }
